1d24e235e7df56cde2092d857a0fce1cab078f66
Chris <cgaber2045@greenvilleschools.us>
2017-11-14 09:57:01 -0500

    Update StudentRecordTester.java

--- StudentRecordTester.java
+++ StudentRecordTester.java
@@ -10,13 +10,13 @@ public class StudentRecordTester
    
    public static void main(String[] args)
    {
-        int scoresa[] = {50, 50, 20, 80, 53};
+        int[] scoresa = {50, 50, 20, 80, 53};
         StudentRecord r1 = new StudentRecord(scoresa);
         
-        int scoresb[] = {20, 50, 50, 53, 80};
+        int[] scoresb = {20, 50, 50, 53, 80};
         StudentRecord r2 = new StudentRecord(scoresb);
         
-        int scoresc[] = {20, 50, 50, 80};
+        int[] scoresc = {20, 50, 50, 80};
         StudentRecord r3 = new StudentRecord(scoresc);
         
         System.out.println(r1.finalAverage());

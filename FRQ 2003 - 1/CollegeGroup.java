796000aa1cdbc2cea402d0250b9cf7ac386e50ab
Chris <cgaber2045@greenvilleschools.us>
2017-11-28 10:31:57 -0500

    Add files via upload

---
+++ FRQ 2003 - 1/CollegeGroup.java
@@ -0,0 +1,56 @@
+
+/**
+ * Write a description of class CollegeGroup here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class CollegeGroup
+{
+    public College[] myColleges;
+    
+    public CollegeGroup() {
+        this.myColleges = new College[7];
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+    public void updateTuition(String collegeName, int newTuition)
+    {
+        for (int i = 0; i < myColleges.length; i++) {
+            if(collegeName.equals(myColleges[i].getName())) {
+                myColleges[i].setTuition(newTuition);
+            }
+        }
+    }
+    
+    public College[] getCollegeList(String region, int low, int high)
+    {
+        int count = 0;
+        for (int i = 0; i < myColleges.length; i++) {
+            if (myColleges[i].getRegion().equals(region) &&
+            myColleges[i].getTuition() >= low &&
+            myColleges[i].getTuition() <= high) {
+                count++;
+            }
+        }
+        
+        College[] result = new College[count];
+        
+        int count2 = 0;
+        for (int i = 0; i < myColleges.length; i++) {
+            if (myColleges[i].getRegion().equals(region) &&
+            myColleges[i].getTuition() >= low &&
+            myColleges[i].getTuition() <= high) {
+                result[count2] = myColleges[i];
+            }
+        }
+        return result;
+    }
+}
---
+++ FRQ 2003 - 1/CollegeGroupTester.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class CollegeGroupTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Arrays;
+
+public class CollegeGroupTester
+{
+     public static void main(String[] args)
+       {
+            College c1 = new College("ColgateUniversity", "Northeast", 27025);
+            College c2 = new College("DukeUniversity", "Southeast", 26000);
+            College c3 = new College("KalamazooCollege", "Midwest", 19764);
+            College c4 = new College("StanfordUniversity", "West", 25917);
+            College c5 = new College("FloridaInternationalUniversity", "Southeast", 10800);
+            College c6 = new College("DartmouthCollege", "Northeast", 27764);
+            College c7 = new College("SpelmanCollege", "Southeast", 11455);
+            
+            CollegeGroup colleges = new CollegeGroup();
+            
+            colleges.myColleges[0] = c1;
+            colleges.myColleges[1] = c2;
+            colleges.myColleges[2] = c3;
+            colleges.myColleges[3] = c4;
+            colleges.myColleges[4] = c5;
+            colleges.myColleges[5] = c6;
+            colleges.myColleges[6] = c7;
+            
+            colleges.updateTuition("ColgateUniversity", 27500);
+            College[] matches = colleges.getCollegeList("Southeast", 10000, 20000);
+            
+            for (int i = 0; i < matches.length; i++) {
+                System.out.println(Arrays.toString(matches));
+            }
+       }
+}

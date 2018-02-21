6bf8af1fece7b664d4df61b8bbbc544b8738c7c2
cgaber2045 <cgaber2045@greenvilleschools.us>
2018-02-21 10:19:12 -0500

    Final

---
+++ 2009/3 - BatteryCharger/BatteryCharger.java
@@ -0,0 +1,61 @@
+
+/**
+ * Write a description of class BatteryCharger here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BatteryCharger
+{
+    
+    /** rateTable has 24 entries representing the charging costs for hours 0 through 23. */
+     private int[] rateTable;
+     
+     public BatteryCharger(int[] rateTable) {
+         this.rateTable = rateTable;
+     }
+     
+    /** Determines the total cost to charge the battery starting at the beginning of startHour.
+     * @param startHour the hour at which the charge period begins
+     * Precondition: 0 ≤ startHour ≤ 23
+     * @param chargeTime the number of hours the battery needs to be charged
+     * Precondition: chargeTime > 0
+     * @return the total cost to charge the battery
+     */
+    
+     private int getChargingCost(int startHour, int chargeTime)
+     { 
+         int time = 0;
+         int cost = 0;
+         
+         for (int i = startHour; i < (startHour + chargeTime); i++) {
+             time = i;
+             while (time > 23) time -= 24;
+             cost += rateTable[time];
+         }
+         
+         return cost;
+     }
+     
+    /** Determines start time to charge the battery at the lowest cost for the given charge time.
+     * @param chargeTime the number of hours the battery needs to be charged
+     * Precondition: chargeTime > 0
+     * @return an optimal start time, with 0 ≤ returned value ≤ 23
+     */
+    
+     public int getChargeStartTime(int chargeTime)
+     { 
+        int lowestCost = Integer.MAX_VALUE;
+        int startCost = 0;
+        
+        for (int i = 0; i < rateTable.length; i++) {
+            if (getChargingCost(i, chargeTime) < lowestCost) {
+                lowestCost = getChargingCost(i, chargeTime);
+                startCost = i;
+            }
+        }
+        
+        return startCost;
+     }
+ 
+} 
